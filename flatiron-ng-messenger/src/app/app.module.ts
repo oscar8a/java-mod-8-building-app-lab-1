a48edd26ebe24515a1b5c775f2ffa93a01f5a0f4
Oscar 8A <oscarochoacerrato@gmail.com>
2022-08-18 14:49:01 -0400

    implement angular components

--- flatiron-ng-messenger/src/app/app.module.ts
+++ flatiron-ng-messenger/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppContactComponent } from './app-contact/app-contact.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { SenderMessageComponent } from './sender-message/sender-message.component';
+import { SendMessageComponent } from './send-message/send-message.component';
+import { UserMessageComponent } from './user-message/user-message.component';
+import { ConversationThreadComponent } from './conversation-thread/conversation-thread.component';
+import { ConversationHistoryComponent } from './conversation-history/conversation-history.component';
+import { ConversationControlComponent } from './conversation-control/conversation-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppContactComponent,
+    ContactListComponent,
+    SenderMessageComponent,
+    SendMessageComponent,
+    UserMessageComponent,
+    ConversationThreadComponent,
+    ConversationHistoryComponent,
+    ConversationControlComponent
   ],
   imports: [
     BrowserModule
